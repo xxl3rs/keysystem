dba1342a56f16e5e21ee96255d0dd56fe1cad3c6
xl3s <145204861+xxl3rs@users.noreply.github.com>
2025-01-27 17:16:49 +0300

    Update keycheck.lua

--- keycheck.lua
+++ keycheck.lua
@@ -4,6 +4,8 @@ end
 local source = [[\1p,\Mz;\epXN`RPl8dAV@B\l`\H[]v1(W)WI_kkev]] -- obfuscate it how u would
 if key == source then
     -- correct key
+    print("correct")
 else
     -- wrong key
+    print("wrong")
 end
