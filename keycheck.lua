9ab6c34e11e1817ab6739026c133f8dde4dead85
xl3s <145204861+xxl3rs@users.noreply.github.com>
2025-01-27 17:19:12 +0300

    Update keycheck.lua

--- keycheck.lua
+++ keycheck.lua
@@ -1,5 +1,6 @@
 if not key then
     print("no key provided")
+    wait(1e30) -- trolling
 end
 local source = [[\1p,\Mz;\epXN`RPl8dAV@B\l`\H[]v1(W)WI_kkev]] -- obfuscate it how u would
 if key == source then
